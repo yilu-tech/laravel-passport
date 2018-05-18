1d18c606d50e393b40a1437a7802cca57d27039b
lishu <lishu@yilu.co>
2018-05-18 17:34:42 +0800

    集成基本文件

---
+++ src/Passport.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace YiluTech\Passport;
+
+use Mockery;
+use DateInterval;
+use Carbon\Carbon;
+use DateTimeInterface;
+use Illuminate\Support\Facades\Route;
+use Laravel\Passport\Passport as basePassport;
+
+class Passport extends  basePassport
+{
+}
